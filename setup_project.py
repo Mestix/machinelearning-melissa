95b446db322c581f1b4d37c15b1ee46f0ff1bb17
Melistixx <32540532+Mestix@users.noreply.github.com>
2025-04-22 17:58:28 +0000

    setup lefthook

--- setup_project.py
+++ setup_project.py
@@ -9,18 +9,13 @@ structure = [
     "presentations",
     "references/codestyle",
     "references/leerdoelen",
-    "reports/figures"
+    "reports/figures",
 ]
 
-files = [
-    "README.md",
-    ".gitignore",
-    ".lefthook.yml",
-    "pyproject.toml"
-]
+files = ["README.md", ".gitignore", ".lefthook.yml", "pyproject.toml"]
 
 for folder in structure:
     os.makedirs(folder, exist_ok=True)
 
 for file in files:
-    open(file, 'a').close()
+    open(file, "a").close()
