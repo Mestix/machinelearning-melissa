cdbdae3909c63d2433ad76cb79efecb495910862
Melistixx <32540532+Mestix@users.noreply.github.com>
2025-05-03 08:26:16 +0000

    lefthook fixes

--- dev/helpers.py
+++ dev/helpers.py
@@ -1,11 +1,12 @@
 import numpy as np
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
+
 def get_last_accuracy_from_tensorboard(logdir, tag="metric/Accuracy"):
     try:
         ea = EventAccumulator(logdir)
         ea.Reload()
-        if tag not in ea.Tags()['scalars']:
+        if tag not in ea.Tags()["scalars"]:
             return np.nan
         events = ea.Scalars(tag)
         if not events:
@@ -14,9 +15,9 @@ def get_last_accuracy_from_tensorboard(logdir, tag="metric/Accuracy"):
     except Exception as e:
         print(f"Fout bij {logdir}: {e}")
         return np.nan
-    
+
 
 def inspect_tensorboard_tags(logdir):
     ea = EventAccumulator(logdir)
     ea.Reload()
-    print("Beschikbare scalar tags:", ea.Tags()['scalars'])
+    print("Beschikbare scalar tags:", ea.Tags()["scalars"])
